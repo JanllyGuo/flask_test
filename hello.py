02c91ace5b0dc5ec2ce9ef31119450cde4b2ebdf
kedugaw <kedugaw@gmail.com>
2022-09-30 00:59:04 +0800

    修改 hello.py 加上 index.html

--- hello.py
+++ hello.py
@@ -3,9 +3,14 @@ https://flask.palletsprojects.com/en/2.2.x/installation/#virtual-environments
 https://flask.palletsprojects.com/en/2.2.x/quickstart/
 '''
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/test")
+def hello_test():
+    return render_template("index.html")
